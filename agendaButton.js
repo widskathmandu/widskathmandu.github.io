dca138e17660415a693b91b68ed82c23e95b79c9
rkhanal <rkhanal@uno.edu>
2021-07-21 19:58:07 -0700

    Added Sections in Agenda

---
+++ agendaButton.js
@@ -0,0 +1,106 @@
+function onButtonClick0(){
+    var test = document.getElementsByClassName("session1")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+
+    test = document.getElementsByClassName("session2")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+
+    test = document.getElementsByClassName("session3")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+
+    test = document.getElementsByClassName("session4")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+}
+function onButtonClick1(){
+    var test = document.getElementsByClassName("session1")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+
+    test = document.getElementsByClassName("session2")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session3")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session4")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+}
+
+function onButtonClick2(){
+    var test = document.getElementsByClassName("session1")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session2")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+
+    test = document.getElementsByClassName("session3")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session4")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+  }
+function onButtonClick3(){
+    var test = document.getElementsByClassName("session1")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session2")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session3")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+
+    test = document.getElementsByClassName("session4")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+}
+function onButtonClick4(){
+    var test = document.getElementsByClassName("session1")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session2")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session3")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "collapse";
+    }
+
+    test = document.getElementsByClassName("session4")
+    for (var i = 0; i<test.length; i ++){
+      test[i].style.visibility = "visible";
+    }
+}
